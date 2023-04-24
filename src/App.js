2c44263538d2aa2ff9cb5b00fcff7e69d1be71a7
br08 <brayan.neves@gmail.com>
2023-04-23 22:26:20 -0300

    added signin and signup components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
-// import Signin from "./components/Signin";
-// import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "auth",
